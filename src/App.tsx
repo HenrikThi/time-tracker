c06b714cd475d7ea32d539a5ad6d2efe185a24b0
Henrik Thiess <34238591+HenrikThi@users.noreply.github.com>
2022-08-30 07:51:50 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, ChangeEvent, useState } from "react";
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import ComputationService, { LogEntry } from "./computation";
 
-function App() {
+export default function App() {
+  const computation = new ComputationService();
+
+  const [logEntries, setLogEntries] = useState<LogEntry[]>([]);
+  const [totalWorked, setTotalWorked] = useState<string>();
+  const [overtime, setOvertime] = useState<string>();
+
+  const startUpload = () => {
+    document.getElementById("upload-input")?.click();
+  };
+
+  const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+    const file = e.target.files[0];
+    const { name } = file;
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      if (!evt?.target?.result) {
+        return;
+      }
+      const { result } = evt.target;
+      const entries = computation.parseFile(result as string);
+      setLogEntries(entries);
+      const total = computation.calculateTotalWork(entries);
+      setTotalWorked(computation.formatDuration(total));
+
+      const overtime = computation.calculateOvertime(entries);
+      setOvertime(computation.formatDuration(overtime));
+    };
+    reader.readAsBinaryString(file);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Fab
+        onClick={() => startUpload()}
+        color="primary"
+        sx={{
+          margin: 0,
+          top: "auto",
+          right: 20,
+          bottom: 20,
+          left: "auto",
+          position: "fixed",
+        }}
+      >
+        <AddIcon />
+        <input
+          id="upload-input"
+          hidden
+          type="file"
+          accept=".csv"
+          onChange={handleFileUpload}
+        />
+      </Fab>
+      <h1>Overtime: {overtime}</h1>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Datum</TableCell>
+              <TableCell align="right">Zeiten</TableCell>
+              <TableCell align="right">Arbeitszeit</TableCell>
+              <TableCell align="right">Pause</TableCell>
+              <TableCell align="right">Notiz</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {logEntries.map((logEntry, idx) => (
+              <TableRow
+                key={idx}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {logEntry.startTime
+                    .format("llll")
+                    .split(" ")
+                    .slice(0, 4)
+                    .join(" ")}
+                </TableCell>
+                <TableCell align="right">
+                  {logEntry.startTime.format("hh:mm")} -{" "}
+                  {logEntry.endTime.format("hh:mm")}
+                </TableCell>
+                <TableCell align="right">
+                  {computation.formatDuration(logEntry.workDuration)}
+                </TableCell>
+                <TableCell align="right">
+                  {computation.formatDuration(logEntry.breakDuration)}
+                </TableCell>
+                <TableCell align="right">{logEntry.notes}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
-
-export default App;
---
+++ src/computation/index.ts
@@ -0,0 +1,148 @@
+import moment, { Moment } from "moment";
+
+export default class ComputationService {
+  parseFile(file: string): LogEntry[] {
+    const logEntries = [];
+
+    const lines = file.split("\n");
+
+    for (let i = 0; i < lines.length; i++) {
+      if (i === 0 || i === lines.length - 1) continue;
+      const logEntry = this.createLogEntry(lines[i]);
+      logEntries.push(logEntry);
+    }
+    console.log(logEntries);
+
+    return logEntries;
+  }
+
+  createLogEntry(line: string): LogEntry {
+    const [day, monthday, times, workDuration, breakDuration, notes] =
+      line.split(",");
+
+    // console.log({ day, monthday, times, workDuration, breakDuration, notes });
+    const [startTime, endTime] = this.getTimes(monthday, times);
+    const logEntry = {
+      startTime: startTime,
+      endTime: endTime,
+      workDuration: this.getDuration(workDuration),
+      breakDuration: this.getDuration(breakDuration),
+      notes: this.parseNotes(notes),
+    };
+    return logEntry;
+  }
+
+  calculateTotalWork(logEntries: LogEntry[]): number {
+    const totalWork = logEntries.reduce((a, c) => a + c.workDuration, 0);
+    console.log(totalWork);
+    return totalWork;
+  }
+
+  formatDuration(s: number) {
+    const ms = s % 1000;
+    s = (s - ms) / 1000;
+    const secs = s % 60;
+    s = (s - secs) / 60;
+    const mins = s % 60;
+    const hrs = (s - mins) / 60;
+
+    return hrs + "h " + mins + "m " + secs + "s ";
+  }
+
+  calculateOvertime(entries: LogEntry[]): number {
+    const PAID_OVERTIME = 1000 * 60 * 60 * 32;
+
+    const weekDays = entries
+      .filter(
+        (entry) =>
+          entry.startTime.format("dddd") !== "Sunday" &&
+          entry.startTime.format("dddd") !== "Saturday"
+      )
+      .map((entry) => entry.startTime.format("L"));
+    const allWorkedDays = new Set(weekDays);
+
+    const normalWorkTime = 1000 * 60 * 60 * 8 * allWorkedDays.size;
+    const realWorkTime = this.calculateTotalWork(entries);
+    const overtime = realWorkTime - normalWorkTime - PAID_OVERTIME;
+
+    console.log(allWorkedDays);
+    console.log(this.formatDuration(normalWorkTime));
+    console.log(this.formatDuration(overtime));
+
+    return overtime;
+  }
+
+  private getTimes(monthDay: string, times: string): [Moment, Moment] {
+    const [startTime, endTime] = times
+      .split(" - ")
+      .map((timeString) => this.formatTime(timeString));
+
+    const date = this.getDate(monthDay);
+    return [moment(`${date}T${startTime}`), moment(`${date}T${endTime}`)];
+  }
+
+  private getDate(monthday: string): string {
+    const [month, day] = monthday
+      .trim()
+      .split(" ")
+      .map((s) => s.replaceAll(".", "").replaceAll('"', "").trim());
+
+    const monthMap = {
+      Jan: "01",
+      Feb: "02",
+      "MÃ¤rz": "03",
+      Apr: "04",
+      Mai: "05",
+      Juni: "06",
+      Juli: "07",
+      Aug: "08",
+      Sep: "09",
+      Oct: "10",
+      Nov: "11",
+      Dez: "12",
+    };
+
+    //@ts-ignore
+    const parsedMonth = monthMap[month];
+    if (!parsedMonth) {
+      throw `month is wrong: ${month}`;
+    }
+    const parsedDay = day.length === 2 ? day : 0 + day;
+    return `2022-${parsedMonth}-${parsedDay}`;
+  }
+
+  private formatTime(time: string): string {
+    return time[1] === ":" ? 0 + time : time;
+  }
+
+  private getDuration(durationString: string): number {
+    const minutes = durationString.match(/.\d?(?=m)/gm);
+    const hrs = durationString.match(/.\d?(?=std)/gm);
+
+    const minutesMs = minutes ? +minutes * 60 * 1000 : 0;
+    const hrsMs = hrs ? +hrs * 60 * 60 * 1000 : 0;
+    return minutesMs + hrsMs;
+  }
+
+  private parseNotes(notes: string): string {
+    return notes.replaceAll("\\", "").replaceAll('"', "");
+  }
+}
+
+export interface LogEntry {
+  startTime: Moment;
+  endTime: Moment;
+  workDuration: number;
+  breakDuration: number;
+  notes: string;
+}
+
+export enum Day {
+  Mon,
+  The,
+  Wed,
+  Thu,
+  Fri,
+  Sat,
+  Sun,
+}
